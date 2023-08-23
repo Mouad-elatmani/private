03a1a0b038a717f2be557e075f92f35d99d50a6a
Mouad-elatmani <96914263+Mouad-elatmani@users.noreply.github.com>
2023-08-23 23:06:00 +0100

    Update main.c

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
  */
 int main(__attribute__((unused)) int argc, char *argv[])
 {
-	if (is_terminal(STDIN_FILENO) == 0)
+	if (isatty(STDIN_FILENO) == 0)
 		run_non_interactive_mode(argv);
 	else
 		run_interactive_shell(argv);
